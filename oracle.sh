9779aa23fe5b34bdcb1ef1ea3f186dbf2cf18762
Alisson Osvaldo <alisson.artigas@gmail.com>
2022-10-26 14:38:08 -0300

    Container oracle utilizando volume

--- oracle.sh
+++ oracle.sh
@@ -1,1 +1,4 @@
-docker run -d --name oracle -p 2020:1521 -e ORACLE_PASSWORD=admin gvenzl/oracle-xe
+docker run -d --name oracle -p 1522:1521 -e ORACLE_PASSWORD=admin gvenzl/oracle-xe
+
+docker create volume oracle-volume
+docker run -d -v oracle-volume:/opt/oracle/oradate --name oracle -p 1522:1521 -e ORACLE_PASSWORD=admin gvenzl/oracle-xe
